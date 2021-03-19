a991986e16cab2891c1247adc6c7983e486bebab
Shah Kibria <shahkibria@Shahs-MacBook-Air.local>
2021-03-19 15:28:05 +0600

    Updating assignment to github.

--- Python_practice.py
+++
@@ -1,4 +0,0 @@
-voting_data = [{"County":"Arapahoe","Registered Voter": 422829},{"County":"Denver","Registered Voter":463353},{"County":"Jefferson","Registered Voter":432438}]
-for x in voting_data:
-    print(x['County']+ " County has "+ str(x["Registered Voter"])+" Registered Voters.")
-
